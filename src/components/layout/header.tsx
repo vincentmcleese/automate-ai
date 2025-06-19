c5fac488a7363e18072c0302914957a462924728
vincent <mcleesevj@gmail.com>
2025-06-19 09:49:59 +0200

    when i click on the ghost team button top left on the refine page it doesnt take me back to home

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -14,7 +14,11 @@ export function Header() {
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container mx-auto flex h-14 items-center">
         {/* Logo */}
-        <Link href="/" className="flex items-center space-x-2">
+        <Link
+          href="/"
+          className="flex items-center space-x-2"
+          onClick={() => (window.location.href = '/')}
+        >
           <Image
             src="/greenghost.png"
             alt="Automate Logo"
